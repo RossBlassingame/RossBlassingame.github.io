c2c5b3dc516a6bc61c2b9ddde9b4e39aff58dbd2
Ross Blassingame <RossBlassingame@users.noreply.github.com>
2017-08-02 09:53:59 -0600

    Delete Projects.js

--- Javascript/Projects.js
+++
@@ -1,9 +0,0 @@
-var projects = [{
-    "title": "iRis",
-    "desc": "An iOS app designed to allow people who are visually-impaired to easily read text found on images.",
-    "url": "https://github.com/mita4829/Iris"
-}, {
-    "title": "The Legend of Harambe",
-    "desc": "A side-scrolling iOS game where the user controls a gorilla and tries to collect bananas while avoiding obstacles.",
-    "url": "https://github.com/ashnaguliani/LegendOfHarambe"
-}];

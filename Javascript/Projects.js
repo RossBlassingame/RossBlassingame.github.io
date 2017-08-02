3a0bb06d605fbb2ea0a59faac1a5d7a648f4597d
Ross Blassingame <RossBlassingame@users.noreply.github.com>
2017-08-02 09:35:56 -0600

    Add files via upload

---
+++ Javascript/Projects.js
@@ -0,0 +1,9 @@
+var projects = [{
+    "title": "iRis",
+    "desc": "An iOS app designed to allow people who are visually-impaired to easily read text found on images.",
+    "url": "https://github.com/mita4829/Iris"
+}, {
+    "title": "The Legend of Harambe",
+    "desc": "A side-scrolling iOS game where the user controls a gorilla and tries to collect bananas while avoiding obstacles.",
+    "url": "https://github.com/ashnaguliani/LegendOfHarambe"
+}];
